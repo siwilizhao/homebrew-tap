152ed0954d693776b278d9a4ecc75bb6e7c3f5e3
Mankong <mankong@mankong.me>
2026-01-28 20:45:47 +0800

    Use prebuilt binary v1.0.0

--- Formula/siwi-download.rb
+++ Formula/siwi-download.rb
@@ -4,17 +4,17 @@
 class SiwiDownload < Formula
   desc "Downloader with pure HTTP implementation supporting breakpoint continuation"
   homepage "https://github.com/rs-videos/siwi-download"
-  url "https://github.com/rs-videos/siwi-download.git", tag: "v1.0.0", revision: "c19327ad5a1d4e234e1aee7343ac83af5e1be527"
   license "MIT"
   version "1.0.0"
 
-  depends_on "rust" => :build
+  url "https://github.com/rs-videos/siwi-download/releases/download/v1.0.0/siwi-download-v1.0.0-aarch64-apple-darwin.tar.gz"
+  sha256 "dfe14c3e53cce0636f277c0ee3744b84400fb85a613ede055d2a2e8b68e710c4"
 
   def install
-    system "cargo", "install", "--root", prefix, "--path", "."
+    bin.install "siwi-download"
   end
 
   test do
-    system "#{bin}/siwi-download", "--version"
+    assert_match version, shell_output("#{bin}/siwi-download --version").strip
   end
 end
